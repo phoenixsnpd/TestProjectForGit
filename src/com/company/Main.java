b8794b1556ceaad24639014bf9b68bb4afec2405
phoenixsnpd <phoenix.snpd@gmail.com>
2021-11-28 17:01:59 +0200

    коммит 1

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        String[] git = {"Это ", "проект ", "для ", "тестирования ", "GitHub"};
+        for (String x : git) {
+            System.out.println(x);
+        }
+    }
+}
